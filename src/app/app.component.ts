039ce4b5582d4e72b140b38c1dc70db84ee15f8e
GRDtechlab <grdtechlab@gmail.com>
2022-04-11 15:34:59 +0530

    #ticketNo 3: commit lint check with husky and pre-push with no-console check

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,7 +8,7 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'clean-code';
   ngOnInt() {
-    console.log('helo world');
+    // console.log('helo world');
   }
   // lets add a comment;
   public run() {
